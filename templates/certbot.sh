5bc351e30eaa99c9a44e0297e8201580f9a4cf57
Metabaron1 <70103029+Metabaron1@users.noreply.github.com>
2020-11-06 00:03:43 +0100

    Update certbot.sh

--- templates/certbot.sh
+++ templates/certbot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/certbot run -n --nginx --agree-tos -m %LETSENCRYPT_EMAIL% -d %MYHOSTNAME% --http-01-port 1080 --https-port 1443
-
+/usr/bin/certbot run -n --nginx --agree-tos -m %LETSENCRYPT_EMAIL% -d %MYHOSTNAME% --http-01-port 8000 --https-port 8443
+sed -i "s/listen 8444;//g" /etc/nginx/conf.d/simplelogin.conf
